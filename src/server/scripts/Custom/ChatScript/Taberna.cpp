f191a6d2c66f5b4d8add776df291976b83c3c834
fmrmx <fmr_@live.com>
2018-06-09 21:56:26 -0500

    Clean Whitespaces

--- src/server/scripts/Custom/ChatScript/Taberna.cpp
+++ src/server/scripts/Custom/ChatScript/Taberna.cpp
@@ -18,9 +18,9 @@ public:
 		msg = (player->GetTeam() == HORDE) ? _UI_FriendsFrame_H + msg : _UI_FriendsFrame_A + msg;
 
     };
-   
+
     void OnChat(Player* player, uint32 /*type*/, uint32 lang, std::string& msg, Channel* channel) override
-    {       
+    {
 		std::string canal = channel->GetName();
 		std::transform(canal.begin(), canal.end(), canal.begin(), ::tolower);
 		if(canal == "taberna")  Faction_Icon(player, msg, lang);
