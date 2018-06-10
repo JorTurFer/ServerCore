0fa12504b94283af87a88b62d27283abcd49f324
fmrmx <fmr_@live.com>
2018-06-09 21:39:57 -0500

    Small Update to Taberna.cpp

--- src/server/scripts/Custom/ChatScript/Taberna.cpp
+++ src/server/scripts/Custom/ChatScript/Taberna.cpp
@@ -8,22 +8,22 @@ class System_Taberna : public PlayerScript
 public:
 	System_Taberna() : PlayerScript("System_Taberna") { }
 
-    void AddIcon(Player* player, std::string& msg, uint32 lang)
+    void Faction_Icon(Player* player, std::string& msg, uint32 lang)
     {
 		if (player->isGMChat()) 
 			return;
-		if (player->GetTeam() == ALLIANCE)
-			msg = "|TInterface/FriendsFrame/PlusManz-Alliance:20:20|t" + msg;
-		else if (player->GetTeam() == HORDE)
-			msg = "|TInterface/FriendsFrame/PlusManz-Horde:20:20|t" + msg;
+        std::string _UI_FriendsFrame_A = "|TInterface/FriendsFrame/PlusManz-Alliance:20:20|t",
+                    _UI_FriendsFrame_H = "|TInterface/FriendsFrame/PlusManz-Horde:20:20|t";
+                    
+		msg = (player->GetTeam() == HORDE) ? _UI_FriendsFrame_H + msg : _UI_FriendsFrame_A + msg;
 
     };
    
     void OnChat(Player* player, uint32 /*type*/, uint32 lang, std::string& msg, Channel* channel) override
-    {
+    {       
 		std::string canal = channel->GetName();
 		std::transform(canal.begin(), canal.end(), canal.begin(), ::tolower);
-		if(canal == "taberna")  AddIcon(player, msg, lang);
+		if(canal == "taberna")  Faction_Icon(player, msg, lang);
     }
 };
 
